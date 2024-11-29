d6e53588af8ba77483957027fb6bad0fe3b820a1
anilciftci <anilciftci04@gmail.com>
2024-11-30 02:58:16 +0300

    Initial Commit

---
+++ BtcturkCase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BtcturkCase
+//
+//  Created by Mehmet Anıl Çiftçi on 30.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
